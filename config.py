138f93a65a5b823dad747a8abee472dc116892c3
Yuri Istomin <yura.ist@icloud.com>
2017-06-21 23:42:26 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,6 @@
+"""This file contains the configuration variables for the app.
+"""
+
+
+# Enable Flask's debugging features. Should be False in production
+DEBUG = True
---
+++ run.py
@@ -0,0 +1,8 @@
+"""The application's entry point. 
+Run this file to start the Flask server and launch the application.
+"""
+from app import app
+
+
+if __name__ == '__main__':
+    app.run()
